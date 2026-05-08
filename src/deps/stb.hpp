2908d09356e1c806ca698a0e79a1c08ccb298860
qbart <git@qbart.dev>
2026-05-09 01:56:10 +0200

    stb

---
+++ src/deps/stb.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+#define STB_IMAGE_RESIZE2_IMPLEMENTATION
+#include "stb/stb_image_resize2.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb/stb_image_write.h"
